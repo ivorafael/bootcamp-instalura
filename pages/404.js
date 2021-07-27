fd7255853e1cc0ee920ebe2415a1cfe09c918aec
Ivo R <ivo.rafael@gmail.com>
2021-07-27 19:36:08 -0300

    ✨  404 page

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+import Title from '../components/Title'
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+const Page404 = () => (
+  <div>
+    <Title>Page not found</Title>
+    <img src="https://i.ytimg.com/vi/gArbocrAO78/hqdefault.jpg" alt="Page not found" />
+  </div>
+)
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+export default Page404
